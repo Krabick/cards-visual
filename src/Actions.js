956cd18eac65afec3ad23d935b86da3a1725c50d
Ivan Khrabolovich <call94@mail.ru>
2022-08-10 16:42:46 +0200

    added new features in Actions

--- src/Actions.js
+++ src/Actions.js
@@ -1,10 +1,14 @@
 import './App.css';
 import React from 'react'
 
+
 function Actions() {
+    const openInNewTab = url => {
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
     return (
         <div className = 'ButtonsStyle'>
-            <button className='ButtonsStyle2'>Открыть</button>
+            <button className='ButtonsStyle2' onClick={() => openInNewTab('https://www.youtube.com/watch?v=dQw4w9WgXcQ')}>Открыть</button>
             <button className='ButtonsStyle2'>Закрыть</button>
 
         </div>
