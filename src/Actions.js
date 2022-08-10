ecf18ddb5e13075d50dc55b1ddb1b7cf81f0827f
Ivan Khrabolovich <call94@mail.ru>
2022-08-10 16:34:05 +0200

    added more Styles and Actions.js

---
+++ src/Actions.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react'
+
+function Actions() {
+    return (
+        <div className = 'ButtonsStyle'>
+            <button className='ButtonsStyle2'>Открыть</button>
+            <button className='ButtonsStyle2'>Закрыть</button>
+
+        </div>
+
+    );
+}
+
+export default Actions;
--- src/Body.js
+++ src/Body.js
@@ -1,16 +1,19 @@
 import './App.css';
-import React from 'react'
+import React from 'react';
+import Actions from './Actions';
 
 function Body({Card}) {
-    console.log({Card})
     return (
-        <div className='BodyStyle'>
-            {Card.map((card) => (
-            <div className='CardsStyle'><h1>{card.name}</h1>
-            <img src={card.avatar}/>
-            <div>{card.description}</div>
-            </div>   ))
-            }
+        <div className='BodyStyleColumn'>
+            <div className='BodyStyleRowForCards'>
+              {Card.map((card) => (
+              <div className='CardsStyle'><h1>{card.name}</h1>
+                <img src={card.avatar}/>
+                <div>{card.description}</div>
+              </div>   ))
+              }
+            </div>
+            <Actions />
         </div>
 
     );
